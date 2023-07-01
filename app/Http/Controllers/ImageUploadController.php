73fdaa40a49803a89c84f8f010889f797c96280c
hadijaman <jaman@gmail.com>
2023-07-02 00:21:31 +0600

    first commit

---
+++ app/Http/Controllers/ImageUploadController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImageUploadController extends Controller
+{
+    //
+
+    public function imageUpload()
+    {
+        return view('imageUpload');
+    }
+
+    public function imageUploadPost(Request $request)
+    {
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+    
+        $imageName = time().'.'.$request->image->extension();  
+     
+        $request->image->move(public_path('images'), $imageName);
+  
+        /* Store $imageName name in DATABASE from HERE */
+    
+        return back()
+            ->with('success','You have successfully upload image.')
+            ->with('image',$imageName); 
+    }
+
+
+}
